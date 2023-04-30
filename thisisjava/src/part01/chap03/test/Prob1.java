b3568e9b0e62643dd528391ddc5aae3fcc589d6c
DJAlysia <lottie_eve@naver.com>
2023-04-30 17:44:37 +0900

    this is java

---
+++ thisisjava/src/part01/chap03/test/Prob1.java
@@ -0,0 +1,13 @@
+package part01.chap03.test;
+
+public class Prob1 {
+
+	public static void main(String[] args) {
+		
+		int x = 10;
+		int y = 20;
+		int z = (++x) + (y--);
+		System.out.println(z);
+	}
+	
+}
---
+++ thisisjava/src/part01/chap03/test/Prob4.java
@@ -0,0 +1,9 @@
+package part01.chap03.test;
+
+public class Prob4 {
+
+	public static void main(String[] args) {
+		int value = 356;
+		System.out.println(value/100*100);
+	}
+}
