d2091a12148e216c21e4aaef88a275d03eeae150
DJAlysia <lottie_eve@naver.com>
2023-05-14 23:48:59 +0900

    code up

---
+++ codeup/src/기초05/_1443.java
@@ -0,0 +1,33 @@
+package 기초05;
+
+import java.util.Scanner;
+
+public class _1443 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] a = new int[n];
+		for (int i = 0; i < n; i++) {
+			a[i] = sc.nextInt();
+		}
+	
+		for (int i = 0; i < n - 1; i++) {
+			int min = i;
+			for (int j = i + 1; j < n; j++) {
+				if (a[j] < a[min]) {
+					min = j;
+				}
+			}
+			int temp = a[i];
+			a[i] = a[min];
+			a[min] = temp;
+		}
+		
+		for (int i = 0; i < n; i++) {
+			System.out.println(a[i]);
+		}
+		sc.close();
+	}
+	
+}
