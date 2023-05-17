c29948d4119256916b280adfffad55908498bf04
DJAlysia <lottie_eve@naver.com>
2023-05-17 09:41:29 +0900

    369 homework

--- human/src/Exam/Answer_369.java
+++ human/src/Exam/Answer_369.java
@@ -1,26 +1,17 @@
 package Exam;
 
-import java.util.Scanner;
-
 public class Answer_369 {
 
 	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int N = 0;
-			try {
-				N = sc.nextInt();
-			} catch (Exception e) {
-				System.err.println("정수를 입력하세요.");
-			}
 			
-			for (int i = 1; i <= N; i++) {
+			for (int i = 1; i <= 100; i++) {
 				int one = i % 10;	
 				int ten = i / 10;	
 				boolean one369 = (one == 3 || one == 6 || one == 9);	
 				boolean ten369 = (ten == 3 || ten == 6 || ten == 9);	
 				
 				if (one369 && ten369) {					
-					System.out.print("**");
+					System.out.println("**");
 				} 
 				else if (one369 || ten369) {					
 					System.out.println("*");	
@@ -29,7 +20,6 @@ public class Answer_369 {
 					System.out.println(i);
 				}
 			}
-			sc.close();
 		}
 	}
 	
